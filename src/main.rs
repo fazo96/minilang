ad64314b05ce0fe2535e8869f8e39890c8ab4f0f
Enrico Fasoli <fazius2009@gmail.com>
2015-10-07 13:32:21 +0200

    added cmd line argument and fixed bug

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate regex;
 use regex::Regex;
 use std::io;
+use std::env;
 use std::io::Read;
 use std::fs::File;
 use std::str::FromStr;
@@ -99,6 +100,7 @@ fn deref(arg : &Refs, mem : &Vec<i32>) -> i32 {
 
 impl Istruction {
     fn execute(&self,pc: &i32,mem: &mut Vec<i32>) -> i32 {
+        println!("Executing: {:?}",self.istruction);
         match &self.istruction {
             &Istructions::Pass => pc + 1,
             &Istructions::Jump { to } => to,
@@ -164,95 +166,97 @@ impl VM {
     fn run(&mut self) {
         self.pc = 1;
         while self.pc >= 1 {
-           if self.code.len() <= (self.pc as usize) { break; }
+           if (self.pc as usize) > self.code.len() { break; }
            println!("Running instruction: {}",self.pc);
            self.pc = self.code[(self.pc - 1) as usize].execute(&self.pc,&mut self.mem); 
+        }
+    }
+    fn load_program(&mut self, program : &str) {
+        let istrn_regex = Regex::new(r"^(\d+): ").unwrap();
+        let halt_regex = Regex::new(r"^halt$").unwrap();
+        let pass_regex = Regex::new(r"^pass$").unwrap();
+        let goto_regex = Regex::new(r"^goto (\d+)$").unwrap();
+        let read_regex = Regex::new(r"^read\((\d+)\)$").unwrap();
+        let write_regex = Regex::new(r"^write\((\d+)\)$").unwrap();
+        let assign_regex = Regex::new(r"^Mem\[(?P<t>\d+)\][ ]*:=[ ]*(?P<a>\d+|Mem\[\d+\])([ ]*(?P<o>\+|-)([ ]*(?P<b>\d+|Mem\[\d+\]))){0,1}$").unwrap();
+        let if_regex = Regex::new(r"^if (?P<a>\d+|Mem\[\d+\])[ ]*(?P<o>>|<|=){1}(?P<p>={0,1})[ ]*(?P<b>\d+|Mem\[\d+\]) then goto (?P<t>\d+)$").unwrap();
+
+        let mem_regex = Regex::new(r"^Mem\[(\d+)\]$").unwrap();
+        let num_regex = Regex::new(r"^\d+$").unwrap();
+
+        self.pc = 0;
+        for line in program.lines() {
+            self.pc = self.pc + 1;
+            let l = line.trim().to_lowercase();
+            println!("({}) Parsing Istruction: {}",self.pc,l);
+            let istrs = istrn_regex.captures(&l).unwrap().at(1).unwrap_or("-1");
+            let istrn = istrs.parse::<i32>().unwrap();
+            if istrn != self.pc {
+                println!("ISTRUCTION: {} HAS INVALID NUMBER",l);
+                break;
+            }
+            let istr : &str = & istrn_regex.replace_all(line,"");
+            println!("({}) Processing: {}",self.pc,istr);
+            if halt_regex.is_match(&istr) { // HALT
+                self.code.push(Istruction { istruction: Istructions::Halt });   
+            } else if pass_regex.is_match(&istr) { // PASS
+                self.code.push(Istruction { istruction: Istructions::Pass });
+            } else if goto_regex.is_match(&istr) { // GOTO
+                let to = goto_regex.captures(&istr).unwrap().at(1).unwrap().parse::<i32>().unwrap();
+                self.code.push(Istruction { istruction: Istructions::Jump { to: to } });
+            } else if write_regex.is_match(&istr) { // WRITE
+                let to = write_regex.captures(&istr).unwrap().at(1).unwrap().parse::<i32>().unwrap();
+                self.code.push(Istruction { istruction: Istructions::Write { index: to } });
+            } else if read_regex.is_match(&istr) { // READ
+                let to = read_regex.captures(&istr).unwrap().at(1).unwrap().parse::<i32>().unwrap();
+                self.code.push(Istruction { istruction: Istructions::Read { index: to } });
+            } else if if_regex.is_match(&istr) { // IF
+                let a_str = if_regex.replace_all(istr,"$a");
+                let b_str = if_regex.replace_all(istr,"$b");
+                let a = conv(&a_str,&mem_regex,&mut self.mem);
+                let b = conv(&b_str,&mem_regex,&mut self.mem);
+                let op_str : &str = & if_regex.replace_all(istr,"$o$p");
+                let oper = get_operator(&op_str);
+                let jmp = if_regex.replace_all(istr,"$t").parse::<i32>().unwrap();
+                self.code.push(Istruction { istruction: Istructions::If { a: a, b: b, condition: oper, jump: jmp } });
+            } else if assign_regex.is_match(&istr) { // ASSIGN
+                let target = assign_regex.replace_all(istr,"$t").parse::<i32>().unwrap();
+                while self.mem.len() <= (target as usize) {
+                    self.mem.push(0);
+                    println!("mem.len() is now {}",self.mem.len());
+                }
+                let a_str = assign_regex.replace_all(istr,"$a");
+                let b_str = assign_regex.replace_all(istr,"$b");
+                let a = conv(&a_str,&mem_regex,&mut self.mem);
+                let op : &str = & assign_regex.replace_all(istr,"$o");
+                let b = conv(&b_str,&mem_regex,&mut self.mem);
+                let oper = match op {
+                    "+" => Operators::Plus { arg: b },
+                    "-" => Operators::Minus { arg: b },
+                    ""  => Operators::Plus { arg: Refs::Literal { value: 0 } },
+                     _  => panic!("Invalid operator")
+                };
+                self.code.push(Istruction { istruction: Istructions::Assignment { index: target, value: a, operator: oper } });
+            } else { // UNKNOWN
+                println!("({}) UNKNOWN ISTRUCTION: {}",self.pc,istr);
+                break;
+            }
         }
     }
 }
 
 fn main() {
-    let mut f = File::open("program.txt").ok().expect("Error opening file");
-    let mut program = String::new();
-    let mut pc = 0;
-    let mut vm = VM::new();
-
-    let istrn_regex = Regex::new(r"^(\d+): ").unwrap();
-    let halt_regex = Regex::new(r"^halt$").unwrap();
-    let pass_regex = Regex::new(r"^pass$").unwrap();
-    let goto_regex = Regex::new(r"^goto (\d+)$").unwrap();
-    let read_regex = Regex::new(r"^read\((\d+)\)$").unwrap();
-    let write_regex = Regex::new(r"^write\((\d+)\)$").unwrap();
-    let assign_regex = Regex::new(r"^Mem\[(?P<t>\d+)\][ ]*:=[ ]*(?P<a>\d+|Mem\[\d+\])([ ]*(?P<o>\+|-)([ ]*(?P<b>\d+|Mem\[\d+\]))){0,1}$").unwrap();
-    let if_regex = Regex::new(r"^if (?P<a>\d+|Mem\[\d+\])[ ]*(?P<o>>|<|=){1}(?P<p>={0,1})[ ]*(?P<b>\d+|Mem\[\d+\]) then goto (?P<t>\d+)$").unwrap();
-
-    let mem_regex = Regex::new(r"^Mem\[(\d+)\]$").unwrap();
-    let num_regex = Regex::new(r"^\d+$").unwrap();
-
-    f.read_to_string(&mut program);
-    for line in program.lines() {
-        pc = pc + 1;
-        let mut code = &mut vm.code;
-        let l = line.trim().to_lowercase();
-        println!("({}) Parsing Istruction: {}",pc,l);
-        let istrs = istrn_regex.captures(&l).unwrap().at(1).unwrap_or("-1");
-        let istrn = istrs.parse::<i32>().unwrap();
-        if istrn != pc {
-            println!("ISTRUCTION: {} HAS INVALID NUMBER",l);
-            break;
-        }
-
-        let istr : &str = & istrn_regex.replace_all(line,"");
-        println!("({}) Processing: {}",pc,istr);
-        if halt_regex.is_match(&istr) { // HALT
-            code.push(Istruction { istruction: Istructions::Halt });   
-        } else if pass_regex.is_match(&istr) { // PASS
-            code.push(Istruction { istruction: Istructions::Pass });
-        } else if goto_regex.is_match(&istr) { // GOTO
-            let to = goto_regex.captures(&istr).unwrap().at(1).unwrap().parse::<i32>().unwrap();
-            code.push(Istruction { istruction: Istructions::Jump { to: to } });
-        } else if write_regex.is_match(&istr) { // WRITE
-            let to = write_regex.captures(&istr).unwrap().at(1).unwrap().parse::<i32>().unwrap();
-            code.push(Istruction { istruction: Istructions::Write { index: to } });
-        } else if read_regex.is_match(&istr) { // READ
-            let to = read_regex.captures(&istr).unwrap().at(1).unwrap().parse::<i32>().unwrap();
-            code.push(Istruction { istruction: Istructions::Read { index: to } });
-        } else if if_regex.is_match(&istr) { // IF
-            let a_str = if_regex.replace_all(istr,"$a");
-            let b_str = if_regex.replace_all(istr,"$b");
-            let a = conv(&a_str,&mem_regex,&mut vm.mem);
-            let b = conv(&b_str,&mem_regex,&mut vm.mem);
-            let op_str : &str = & if_regex.replace_all(istr,"$o$p");
-            let oper = get_operator(&op_str);
-            let jmp = if_regex.replace_all(istr,"$t").parse::<i32>().unwrap();
-            code.push(Istruction { istruction: Istructions::If { a: a, b: b, condition: oper, jump: jmp } });
-        } else if assign_regex.is_match(&istr) { // ASSIGN
-            let target = assign_regex.replace_all(istr,"$t").parse::<i32>().unwrap();
-            while vm.mem.len() <= (target as usize) {
-                vm.mem.push(0);
-                println!("mem.len() is now {}",vm.mem.len());
-            }
-            let a_str = assign_regex.replace_all(istr,"$a");
-            let b_str = assign_regex.replace_all(istr,"$b");
-            let a = conv(&a_str,&mem_regex,&mut vm.mem);
-            let op : &str = & assign_regex.replace_all(istr,"$o");
-            let b = conv(&b_str,&mem_regex,&mut vm.mem);
-            let oper = match op {
-                "+" => Operators::Plus { arg: b },
-                "-" => Operators::Minus { arg: b },
-                ""  => Operators::Plus { arg: Refs::Literal { value: 0 } },
-                 _  => panic!("Invalid operator")
-            };
-            code.push(Istruction { istruction: Istructions::Assignment { index: target, value: a, operator: oper } });
-        } else { // UNKNOWN
-            println!("({}) UNKNOWN ISTRUCTION: {}",pc,istr);
-            break;
-        }
-    }
-    println!("--- ISTRUCTIONS");
-    for i in &vm.code {
-        println!("{:?}",i.istruction);
-    }
-    println!("--- EXECUTING...");
-    vm.run()
+    match env::args().nth(1) {
+        Some(file_name) => {
+            let mut f = File::open(file_name).ok().expect("Error opening file");
+            let mut program = String::new();
+            let mut vm = VM::new();
+            f.read_to_string(&mut program);
+            vm.load_program(&program);
+            vm.run()
+        },
+        None => {
+            println!("Usage: {} <filepath>",env::args().nth(0).unwrap())
+        } 
+    };
 }
